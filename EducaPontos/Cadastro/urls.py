6bbdf9b1364d9a9e2ed0fb8b0400e6ab4da77b13
Ian-Di <iandigori12@gmail.com>
2024-11-25 21:22:13 -0300

    Inserindo paginas home e cadastro

--- EducaPontos/Cadastro/urls.py
+++ EducaPontos/Cadastro/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import cadastro
 
 urlpatterns = [
-    path('home/', home),
+    path('', cadastro, name='cadastro'),
 ]
